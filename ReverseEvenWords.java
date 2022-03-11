fbad6dd69e5f7ba3322d08978dc2b4851941491c
MohanapriyaPonnusamy <100363431+MohanapriyaPonnusamy@users.noreply.github.com>
2022-03-11 12:16:04 +0400

    Update ReverseEvenWords.java

--- ReverseEvenWords.java
+++ ReverseEvenWords.java
@@ -7,16 +7,16 @@ public class ReverseEvenWords {
 		String[] split = test.split(" ");
        for (int i = 0; i < split.length; i++) {
 		    if((i)%2==0) {
-		    System.out.println(split[i]);
-		    System.out.println(" ");
+		    System.out.print(split[i]);
+		    System.out.print(" ");
 		}
 		   else {
 		    char[] charArray = split[i].toCharArray();
 		     for (int j =charArray.length-1 ; j>=0;  j--) {
-		        System.out.println(charArray[j] );
+		        System.out.print(charArray[j] );
 		        
 		   }
-		    System.out.println(" ");
+		    System.out.print(" ");
 		    
 		}
 		    
